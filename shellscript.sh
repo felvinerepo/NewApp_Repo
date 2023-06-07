c7a25b0b8470243a863db8ff5d8ca5f67d8ace2a
felvinerepo <133920636+felvinerepo@users.noreply.github.com>
2023-06-07 02:25:48 +0100

    Create shellscript.sh

---
+++ shellscript.sh
@@ -0,0 +1,7 @@
+uname
+                      java -version
+                      mkdir SGITech
+                      cd SGITech
+                      touch myfile.java
+                      echo "import module , this is the java code" > myfile.java
+                      cat myfile.java
